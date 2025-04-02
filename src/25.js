9020a6b0b03ce3139fe5d52043d6d601bc9faecc
cristiaanavocados <cristiaanavocados@gmail.com>
2025-04-02 02:44:46 +0000

    Add 25.js

---
+++ src/25.js
@@ -0,0 +1,12 @@
+let nodes = [
+  {
+    name: "Node.js",
+    description: "A server-side JavaScript runtime.",
+    github: "https://github.com/joyent/node"
+  },
+  {
+    name: "Express",
+    description: "A web framework for building scalable applications.",
+    github: "https://github.com/expressjs/express"
+  }
+];
